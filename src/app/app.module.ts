18aac25348a2804d8bee4e9b7a822d069f60e74d
zaanton <ziccardiantonio@outlook.com>
2021-12-15 12:15:01 -0500

    Made Dashboard!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { GiftGivingComponent } from './components/gift-giving/gift-giving.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    GiftGivingComponent
+    GiftGivingComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
