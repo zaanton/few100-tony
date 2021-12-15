154f385031a0039c07d73280487ed262fe383f63
zaanton <ziccardiantonio@outlook.com>
2021-12-15 12:12:52 -0500

    Make Gift Giving Component!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GiftGivingComponent } from './components/gift-giving/gift-giving.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GiftGivingComponent
   ],
   imports: [
     BrowserModule,
